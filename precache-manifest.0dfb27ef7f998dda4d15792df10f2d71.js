b330e8181e8752b51b125a96120c415c112d525c
TareqJudeh75 <tareq.joudeh@gmail.com>
2020-01-06 01:08:00 +0200

    Updates

---
+++ precache-manifest.0dfb27ef7f998dda4d15792df10f2d71.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2162432d4c0350ed165ce81eeb160dbd",
+    "url": "/react-robo-monsters/index.html"
+  },
+  {
+    "revision": "6ce31631f09d63648895",
+    "url": "/react-robo-monsters/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "599b480266c3ed353b5c",
+    "url": "/react-robo-monsters/static/css/main.7cfddd64.chunk.css"
+  },
+  {
+    "revision": "6ce31631f09d63648895",
+    "url": "/react-robo-monsters/static/js/2.b92601de.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/react-robo-monsters/static/js/2.b92601de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "599b480266c3ed353b5c",
+    "url": "/react-robo-monsters/static/js/main.e299e37c.chunk.js"
+  },
+  {
+    "revision": "436dcccb195b42734d94",
+    "url": "/react-robo-monsters/static/js/runtime-main.b489b554.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-robo-monsters/static/media/logo.5d5d9eef.svg"
+  }
+]);
